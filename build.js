e5b6db88c914c50c7477ecb66ce9fd43e2b64079
Saeid Mohadjer <saeid@fastmail.fm>
2026-03-13 20:24:10 +0100

    adds v2

--- build.js
+++
@@ -1,9 +0,0 @@
-import esbuild from "esbuild";
-import sassPlugin from "esbuild-plugin-sass";
-
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  outdir: "dist",
-  plugins: [sassPlugin()],
-});
